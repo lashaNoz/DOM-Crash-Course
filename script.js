5136b746d429ba88c8c11c406a5e132d17582b6e
unknown <George>
2020-02-29 14:50:44 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,115 @@
+// const heading = document.getElementById("heading");
+// console.log(heading);
+// console.log(typeof heading);
+
+// const heading2 = document.getElementById("heading");
+// console.log(heading2);
+
+// const list = document.getElementById("list");
+
+// list.textContent = "Hi there";
+
+// console.log(list.textContent);
+
+// list.textContent = "<h1>Hi there</h1>";
+
+// console.log(list.innerHTML);
+
+// const list = document.querySelector(".list");
+// console.log(list);
+
+// const li = document.querySelector("li");
+// console.log(li);
+
+// const lis = document.querySelectorAll("li");
+// console.log(lis);
+
+// const listItems = document.querySelectorAll(".list-item");
+// console.log(listItems);
+
+// const heading = document.getElementById("heading");
+// heading.style.color = "red";
+// heading.style.backgroundColor = "black";
+
+// heading.className = "changeBG";
+// heading.className = "changeFT";
+// heading.className += " changeFT";
+
+// console.log(heading.classList);
+
+// heading.classList.add("changeCL");
+// heading.classList.remove("changeCL");
+
+// const lis = document.querySelectorAll("li");
+// console.log(lis);
+
+// lis[1].style.backgroundColor = "red";
+
+// for (let i = 0; i < lis.length; i++) {
+//   lis[i].style.backgroundColor = "royalblue";
+// }
+
+// lis[0].style.cssText = "background-color: coral; color: white; font-size: 25px";
+
+// const heading = document.querySelector(".heading");
+// const btn = document.querySelector(".btn");
+
+// btn.onclick = () => {
+//   console.log("Clicked!");
+// };
+
+// btn.onmouseover = () => {
+//   heading.style.cssText = "background-color: brown; color: white";
+// };
+
+// btn.onmouseout = () => {
+//   heading.style.cssText = "background-color: transparent; color: black";
+// };
+
+// const clickButton = () => {
+//   console.log("Clicked!");
+// };
+
+// btn.addEventListener("click", e => {
+//   heading.style.cssText = "background-color: brown; color: white";
+// console.log(e.target);
+// });
+
+// const paragraph = document.querySelector(".paragraph");
+// console.log(paragraph.getAttribute("id"));
+// console.log(paragraph.getAttribute("class"));
+// console.log(paragraph.getAttribute("title"));
+
+// paragraph.setAttribute("style", "background-color: coral");
+
+// paragraph.removeAttribute("style");
+
+// console.log(paragraph.hasAttribute("style"));
+// console.log(paragraph.hasAttribute("class"));
+
+// const listItem = document.getElementById("list-item");
+// console.log(listItem.parentElement);
+// console.log(listItem.parentNode.parentNode);
+
+// const list = document.querySelector(".list");
+// console.log(list.childNodes);
+// console.log(list.children);
+// console.log(list.firstElementChild);
+
+// console.log(listItem.nextElementSibling);
+
+// const newEl = document.createElement("li");
+
+// const text = document.createTextNode("Blog");
+
+// newEl.appendChild(text);
+
+// console.log(newEl);
+
+// list.appendChild(newEl);
+
+// list.insertBefore(newEl, list.children[1]);
+
+// list.removeChild(newEl);
+
+// console.log(list);
